f4f8d53d3758c2b1aa5949658e5e536a8ef13e6e
doodeec <doodeec@gmail.com>
2014-04-05 13:03:11 +0200

    some new shit

---
+++ app/src/main/java/com/glassy/GlassyActivity.java
@@ -0,0 +1,95 @@
+package com.glassy;
+
+import com.glassy.utils.MathUtils;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * This activity manages the options menu that appears when the user taps on the compass's live
+ * card.
+ */
+public class GlassyActivity extends Activity {
+
+    private CompassService.CompassBinder mCompassService;
+    private boolean mResumed;
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            if (service instanceof CompassService.CompassBinder) {
+                mCompassService = (CompassService.CompassBinder) service;
+                openOptionsMenu();
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            // Do nothing.
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        bindService(new Intent(this, CompassService.class), mConnection, 0);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mResumed = true;
+        openOptionsMenu();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mResumed = false;
+    }
+
+    @Override
+    public void openOptionsMenu() {
+        if (mResumed && mCompassService != null) {
+            super.openOptionsMenu();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.compass, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.read_aloud:
+                mCompassService.readHeadingAloud();
+                return true;
+            case R.id.stop:
+                stopService(new Intent(this, CompassService.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onOptionsMenuClosed(Menu menu) {
+        super.onOptionsMenuClosed(menu);
+
+        unbindService(mConnection);
+
+        // We must call finish() from this method to ensure that the activity ends either when an
+        // item is selected from the menu or when the menu is dismissed by swiping down.
+        finish();
+    }
+}
